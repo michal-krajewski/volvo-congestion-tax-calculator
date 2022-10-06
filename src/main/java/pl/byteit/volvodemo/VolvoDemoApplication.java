b462924ede5c4e4735b645987fe16c0cd84dc3b6
Michał Krajewski <Krajewski.M@protonmail.com>
2022-10-06 19:54:50 +0200

    Init

---
+++ src/main/java/pl/byteit/volvodemo/VolvoDemoApplication.java
@@ -0,0 +1,13 @@
+package pl.byteit.volvodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolvoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolvoDemoApplication.class, args);
+	}
+
+}
